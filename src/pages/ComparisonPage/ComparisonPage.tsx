93237dcd3a8e1b7443564e000543e104b3ca2a26
keydmitrij-fe <241766034+keydmitrij-fe@users.noreply.github.com>
2026-01-23 13:45:15 +0300

    refactor: remove unused constants and console.log

--- src/pages/ComparisonPage/ComparisonPage.tsx
+++ src/pages/ComparisonPage/ComparisonPage.tsx
@@ -3,11 +3,6 @@ import "./ComparisonPage.scss"
 import ProductCard from "../../components/ProductCard/ProductCard"
 
 const ComparisonPage: React.FC = () => {
-  console.log(productsMock)
-  const name = productsMock[0].name
-  const img = productsMock[0].imageUrl
-  const sku = productsMock[0].sku
-
   return (
     <div className="comparison">
       {productsMock.map((product) => (
