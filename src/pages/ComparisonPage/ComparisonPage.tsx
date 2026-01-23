2012e992aeb5464fbb2efb7cd12e2e9a7ca7ce36
keydmitrij-fe <241766034+keydmitrij-fe@users.noreply.github.com>
2026-01-23 14:06:38 +0300

    feat(ui): add static items to comparison page

--- src/pages/ComparisonPage/ComparisonPage.tsx
+++ src/pages/ComparisonPage/ComparisonPage.tsx
@@ -5,15 +5,28 @@ import ProductCard from "../../components/ProductCard/ProductCard"
 const ComparisonPage: React.FC = () => {
   return (
     <div className="comparison">
-      {productsMock.map((product) => (
-        <ProductCard
-          key={product.id}
-          imageUrl={product.imageUrl}
-          name={product.name}
-          sku={product.sku}
-          selected
-        />
-      ))}
+      <div className="comparison__available">
+        {productsMock.map((product) => (
+          <ProductCard
+            key={product.id}
+            imageUrl={product.imageUrl}
+            name={product.name}
+            sku={product.sku}
+            selected={false}
+          />
+        ))}
+      </div>
+      <div className="comparison__selected">
+        {productsMock.map((product) => (
+          <ProductCard
+            key={product.id}
+            imageUrl={product.imageUrl}
+            name={product.name}
+            sku={product.sku}
+            selected={true}
+          />
+        ))}
+      </div>
     </div>
   )
 }
